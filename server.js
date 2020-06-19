5d52a30bd8b1041a54b4e7638c4bca12ed6ac423
lauryngrootfaam <grootfaamlauryn@gmail.com>
2020-06-19 16:38:12 +0200

    fixed sessions & fixed bugs with database. There is another bug that i need to fix though

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ console.log('Server is starting')
 const express = require('express')
 const app = express()
 const path = require('path')
-const slug = require('slug')
+// const slug = require('slug')
 const bodyParser = require('body-parser')
 const session = require('express-session')
 
@@ -42,24 +42,6 @@ express()
 
   .listen(8000, listening)
 
-// object met data (array) over verschillende gebruikers
-// deze data moet uiteindelijk uit de database komen
-// const users = [
-//   {
-//     username: 'lauryngr_',
-//     firstname: 'Lauryn',
-//     age: 20,
-//     description: 'ik hou van ...'
-//   },
-
-//   {
-//     username: 'Jay24',
-//     firstname: 'Jay',
-//     age: 24,
-//     description: 'ik hou van voetbal...'
-//   }
-// ]
-
 
 //connecten met de database
 let db;
@@ -74,61 +56,56 @@ mongodb.MongoClient.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopol
 });
 
 
-//Het zoeken van een db collectie en een array daarin
+async function addProfile (req, res, next) {
+  // let allUsers = await db.collection('usersInfo').find().toArray()
 
-function hoofdpagina (req, res, next) {
-  
-  db.collection('usersInfo').find().toArray(done)
+  req.session.user = {
+    username: req.body.username,
+    firstname: req.body.firstname,
+    age: req.body.age,
+    description: req.body.description
+  }
 
+  db.collection('usersInfo').insertOne(
+  req.session.user
+  )
 
-  function done (err, users){
-    if (err) {
-          next (err)
-          }  
-  else if (!req.session.user) {
-          res.status(401).send('i got this bitch fr')
-        return
-    
-        } 
-        else {
-          console.log("dont give up, your this close")
-          // res.render('hoofdpagina.ejs', {
-          //        data: users
+  let allUsers = await db.collection('usersInfo').find().toArray()
+    done();
 
-          //       })
-                res.render('hoofdpagina.ejs', { data: req.body })
-        }
-      }
-  
-}
-
-//Het pushen van de input van gebruikers naar database 
-//account maken
-
-//ik gebruik hier slug zodat ik username kan declaren buiten de insertOne functie en kan gebruiken voor session
-  // const username = slug(req.body.username)
-  //hulp gehad van Zoë
-
-
-function addProfile (req, res, next) {
-  //ik gebruik hier slug zodat ik username kan declaren buiten de insertOne functie en kan gebruiken voor session
-  // const username = slug(req.body.username)
-  db.collection('usersInfo').insertOne({
-  username: req.body.username,
-  firstname: req.body.firstname,
-  age: req.body.age,
-  description: req.body.description
-  }, done)
-
+  console.log('req session user @105 ', req.session.user)
   function done(err) {
     if (err) {
       next(err)
        } 
     else {
-        res.render('hoofdpagina.ejs', { data: req.body })
+        res.render('hoofdpagina.ejs', { data: allUsers })
     }
   }
 }
+
+async function hoofdpagina (req, res, next) {
+  let allUsers = await db.collection('usersInfo').find().toArray()
+  done();
+  // res.render('hoofdpagina.ejs', {data: allUsers})
+  // console.log('collection', await db.collection('usersInfo').find().toArray(done));
+// const allUsers = db.collection('usersInfo').find().toArray(done);
+  function done (err, users){
+    if (err) {
+          next (err)
+          } 
+  else if (!req.session.user){
+      res.status(401).send('Login first')
+    }
+ 
+        else {
+          console.log("dont give up, your this close", req.session.user)
+              res.render('hoofdpagina.ejs', {data: allUsers})
+        }
+      }
+  }
+
+  
 
 
 //sessions
@@ -145,10 +122,6 @@ else {
 res.sendFile(path.join(__dirname + '/view/uitloggen.ejs'));
 res.render('uitloggen', {viewCount : req.session.viewCount})
 }
-
- //  else if (!req.session.user){
-    //   res.status(401).send('Credentials required')
-    // }
 
 // functie die feedback geeft voor mijzelf dat de server daadwerkelijk "luistert", ik vind dit super fijn.
 function listening() {  
