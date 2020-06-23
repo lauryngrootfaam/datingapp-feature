877a3572980a9bd83fa835d0db23ac26bf3d460a
lauryngrootfaam <grootfaamlauryn@gmail.com>
2020-06-23 22:41:11 +0200

    fixed pwd hashing

--- server.js
+++ server.js
@@ -3,19 +3,23 @@ console.log('Server is starting')
 
 // constanten die de express functionaliteiten gebruikt
 const express = require('express')
-const app = express()
 const path = require('path')
-// const slug = require('slug')
 const bodyParser = require('body-parser')
 const session = require('express-session')
+const mongodb = require('mongodb')
 
-// constanten ejs
-const ejs = require('ejs')
+
+//password hashing
+const bcrypt = require('bcrypt');
+var saltRounds = 10;
+
+// ejs requiren voor templating
+require('ejs')
 
 //database
 //beveiligde info storen in .env file
 require('dotenv').config()
-const mongodb = require('mongodb')
+
 //mongoDB url
 const mongodbUrl = process.env.DB_URL
 
@@ -45,7 +49,6 @@ express()
  
   .listen(8000, listening)
 
-
 //connecten met de database
 let db;
 mongodb.MongoClient.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopology: true }, (err, client) => {
@@ -59,53 +62,73 @@ mongodb.MongoClient.connect(mongodbUrl, { useNewUrlParser: true, useUnifiedTopol
 });
 
 function inschrijven (req, res){
-  res.sendFile(path.join(__dirname + '/view/inschrijven.ejs'));
   res.render('inschrijven.ejs');
 }
 
+//in deze functie wordt de input van de gebruiker in de db aangemaakt
+//Er wordt voor elke gebruiker een session gemaakt
+// En met functie done wordt er gecontroleerd op foutmeldingen en redirect hij naar de juiste pagina
+
+//hulp gehad van Roeland met de sessions (samen met Zoë)
 function inschrijfData (req, res){
+  
   req.session.user = {
         username: req.body.username,
         firstname: req.body.firstname,
         age: req.body.age,
-        description: req.body.description
+        description: req.body.description,
+        password : req.body.password
       }
-    
+
       db.collection('usersInfo').insertOne(
-      req.session.user, done)
-
-     function done(err) {
-            if (err) {
-              next(err)
-               } 
-            else {
-               res.redirect('update')
-              }
-          }
-}
+        req.session.user)
 
 
-async function updatePagina(req, res) { //async function because promise (user_id) was pending
-	let currentUser = await db.collection('usersInfo').findOne({'_id': mongodb.ObjectID(req.session.user._id)}); //stored globally for re-use
+      let password =  req.session.user.password;
+
+    //insertOne heb ik van de CRUD handleiding van mongodb
+    bcrypt.genSalt(saltRounds, function(err, salt){
+        bcrypt.hash(password, salt, function(err, hash) {
+          if(err) throw err;
+          req.session.user.password = hash;
+      
+        });
+    })
+      // db.collection('usersInfo').insertOne(
+      // req.session.user, done)
+
+console.log("@97 password: ", req.body.password)
+res.redirect('update')}
+//      function done(err) {
+//             if (err) {
+//               next(err)
+//                } 
+//             else {
+//                res.redirect('update')
+//               }
+//           }
+// }
+
+//Deze functie zorgt ervoor dat ik een specifiek gebruiker uit de database kan ophalen (sessions)
+// en deze data kan aanroepen in mijn ejs templating files
+//ook met deze async functie ben ik geholpen door Vincent en Roeland
+
+async function updatePagina(req, res) {
+	let currentUser = await db.collection('usersInfo').findOne({'_id': mongodb.ObjectID(req.session.user._id)});
 	res.render('update', {data: currentUser});
 }
 
-
-// async function hoofdpagina (req, res){
-//   let currentUser = await db.collection('usersInfo').findOne({'_id': mongodb.ObjectID(req.session.user._id)}); //stored globally for re-use
-// 	res.render('hoofdpagina', {data: currentUser});
-// }
-
+//Bij deze functie zoekt hij ook alle gebruikers uit de db. Ik store het in een variabele zodat ik deze
+// kan aan roepen in mijn ejs templating files
 async function account (req, res){
-  let currentUser = await db.collection('usersInfo').findOne({'_id': mongodb.ObjectID(req.session.user._id)}); //stored globally for re-use
+  let currentUser = await db.collection('usersInfo').findOne({'_id': mongodb.ObjectID(req.session.user._id)}); 
   let allUsers = await db.collection('usersInfo').find().toArray() 
   res.render('account', {data: currentUser, users: allUsers});
 }
 
-// async function hoofdpagina (res){
-//   let allUsers = await db.collection('usersInfo').find().toArray()
-// 	res.render('account', {users: allUsers});
-// }
+//ik zoek hier naar een element (username) uit de huidige gebruiker (session), 
+//die ik vervolgens met .updateOne kan updaten in mijn db
+//update functie is vanuit de slides van de backend lessen
 
 function addProfileUpdate(req, res) {
 	db.collection('usersInfo').updateOne({
@@ -113,9 +136,9 @@ function addProfileUpdate(req, res) {
 	{$set: 
 			{username: req.body.username}
 	}
-	, check);
+	, done);
 
-	function check(err, data) {
+	function done(err) {
 		if (err) {
 			next(err);
 		} else {
@@ -124,12 +147,15 @@ function addProfileUpdate(req, res) {
 	}
 }
 
+//Deze functie delete de huidige session/gebruiker uit de db.
+//De functie wordt deleteAccount wordt nog gecheckt op foutmeldingen door functie done
+//delete functie is vanuit de slides van de backend lessen
 function deleteAccount(req, res) {
 	db.collection('usersInfo').deleteOne({
 		'_id': mongodb.ObjectID(req.session.user._id)
-	}, check);
+	}, done);
 
-	function check(err, data) {
+	function done(err, data) {
 		if (err) {
 			next(err);
 		} else {
@@ -138,8 +164,7 @@ function deleteAccount(req, res) {
 	}
 }
 
-
-//sessions
+// research naar sessions
 // bron: https://www.youtube.com/watch?v=hKYjSgyCd60
 
 // function logout(req, res, next) {
@@ -161,20 +186,11 @@ function listening() {
 
 // een functie die hij uitvoert zodra er een request wordt opgevraagd door de gebruiker (route)
 function onhome (req, res){
-  // res.send('index.html')
   res.render('index', {data: {userQuery: req.params.userQuery}})
 }
 
-// functies pagina's server on request
-// function inschrijven (req, res){
-//   res.sendFile(path.join(__dirname + '/view/inschrijven.ejs'));
-//   res.render('inschrijven.ejs');
-
-// }
-
 function inloggen (req, res){
-  res.sendFile(path.join(__dirname + '/view/inloggen.ejs'));
-  res.render('inloggen.ejs');
+   res.render('inloggen.ejs');
 }
 
 
